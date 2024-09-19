cdf984892fb08790d74e2749044f23067394cee6
Rajsinh123 <rajsinhmandawale15@gmail.com>
2024-09-19 13:08:53 +0530

    heading change

---
+++ Enum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    class Program
+    {
+        enum level
+        {
+            high,medium,low
+        }
+        static void Main(string[] args)
+        {
+            level m = level.high;
+            Console.WriteLine("m");
+            Console.ReadLine();
+
+        }
+    }
+}
